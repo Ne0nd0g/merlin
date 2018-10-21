2c693f3df8b43761a5af0d3200aa20107a812508
Ne0nd0g <Russel.VanTuyl@gmail.com>
2018-10-21 17:56:56 -0400

    Updated version number

--- pkg/merlin.go
+++ pkg/merlin.go
@@ -18,7 +18,7 @@
 package merlin
 
 // Version is a constant variable containing the version number for the Merlin package
-const Version = "0.6.2.BETA"
+const Version = "0.6.3.BETA"
 
 // Build is the unique number based off the git commit in which it is compiled against
 var Build = "nonRelease"
